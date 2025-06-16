4594ac1851e217a0a923c943355901deb5e1d7e1
Justin Sato <justinsccs1@gmail.com>
2025-06-16 16:25:18 -0700

    updated to log daily close
    minute data is not available or times before 7 days prior. Thus, the code has been updated to check daily close from 2023 to cross check with target label from the DRW dataset.

--- btcpricesminute.py
+++ btcpricesminute.py
@@ -4,11 +4,11 @@ import pandas as pd
 import datetime
 
 cryptocompare.cryptocompare._set_api_key_parameter("5c5ef1da79a49d4ba3c4d14b62ac811646759e02a8d491ca848102a82dc6f9fb")
-btcdata = cryptocompare.get_historical_price_minute("BTC", currency="USD", limit=1799, toTs=datetime.datetime(year=2023, month=3, day=2, hour=6, minute=0))
+btcdata = cryptocompare.get_historical_price_day("BTC", currency="USD", limit=59, toTs=datetime.datetime(year=2023, month=4, day=29))
 
 
-price = [entry['close'] for entry in btcdata['Data']['Data']]
-arr = np.arrange(1, 1801)
+price = [entry['close'] for entry in btcdata]
+arr = np.arange(1, 61)
 myList = arr.tolist()
 
 data = {'timeStamp': myList,
